66844086c2544c6435bbd95554a80a7edbcff223
113876-PAOLUCCI-AGUSTIN <113876@tecnicatura.frc.utn.edu.ar>
2024-03-24 00:24:57 -0300

    Enunciado cargado.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+/*
+* 10. Ingresar un número y validar que sea positivo.
+* Si no lo es continuar solicitándolo al usuario hasta que ingrese un número correcto.
+* */
+
 public class Main {
     public static void main(String[] args) {
-        
+
     }
 }
