d88f403c8d6fd3a3e1a1b5f260a0dd4b20079d7c
113876-PAOLUCCI-AGUSTIN <113876@tecnicatura.frc.utn.edu.ar>
2024-03-24 00:32:00 -0300

    Ciclo While creado.

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,17 @@
 * Si no lo es continuar solicitándolo al usuario hasta que ingrese un número correcto.
 * */
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Ingrese un número. Chequearé si su numero es negativo.");
+        Scanner input = new Scanner(System.in);
+        Integer numeroIngresado = input.nextInt();
+        while(numeroIngresado<=0){
+            System.out.println("Debe ingresar un numero POSITIVO...");
+            numeroIngresado = input.nextInt();
+        }
+        System.out.println("Finalmente! El número que ingresó es positivo! :)");
     }
 }
