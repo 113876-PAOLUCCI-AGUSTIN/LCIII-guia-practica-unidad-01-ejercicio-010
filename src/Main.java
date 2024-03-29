a6b00d2209b85e8da18d83edc95849270db2cfad
113876-PAOLUCCI-AGUSTIN <113876@tecnicatura.frc.utn.edu.ar>
2024-03-29 17:02:18 -0300

    Solved.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
 /*
-* 10. Ingresar un número y validar que sea positivo.
-* Si no lo es continuar solicitándolo al usuario hasta que ingrese un número correcto.
-* */
+ * 10. Ingresar un número y validar que sea positivo.
+ * Si no lo es continuar solicitándolo al usuario hasta que ingrese un número correcto.
+ *
+ */
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Ingrese un número. Chequearé si su numero es negativo.");
+        System.out.println("Ingrese un número. Validaré que su número sea POSITIVO...");
         Scanner input = new Scanner(System.in);
         Integer numeroIngresado = input.nextInt();
+
         while(numeroIngresado<=0){
             System.out.println("Debe ingresar un numero POSITIVO...");
             numeroIngresado = input.nextInt();
